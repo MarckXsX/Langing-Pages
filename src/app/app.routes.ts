6aa8d69953118e28ed98153f4314635d1188dfae
Marco Gerardo Serrano Lopez <marcolopez121@outlook.com>
2025-04-29 20:27:14 -0600

    Redireccion a Componente no encontrado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { CambioDiagnosticoComponent } from './components/cambio-diagnostico/cambio-diagnostico.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path: 'cambio-diagnostico',
         component: CambioDiagnosticoComponent
+    },
+    {
+        path: '**',
+        component: NotFoundComponent
     }
 ];
